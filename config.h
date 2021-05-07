d4a655013eb5b88be5d8b0d5453cba7d77b2267e
Aditya Kshirsagar <kaditya.anil@gmail.com>
2021-05-07 13:23:14 +0530

    patched fullscreen

--- config.h
+++ config.h
@@ -75,10 +75,11 @@ static Key keys[] = {
 	//{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, /* float */
+	//{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, /* float */
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /* monocle */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_g,              shiftview,           { .i = -1 } },
