893d1b59824d759f7837a3b412f3824874ef6e83
Aditya Kshirsagar <kaditya.anil@gmail.com>
2021-05-09 12:14:24 +0530

    patched sticky windows

--- config.def.h
+++ config.def.h
@@ -134,6 +134,7 @@ static Key keys[] = {
 	//{ MODKEY,	                XK_i,      setlayout,      {.v = &layouts[4]} }, /* bstackhoriz */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                 	XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 
 	/*tags */
